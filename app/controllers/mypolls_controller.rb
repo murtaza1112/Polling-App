b8dfd09be275af5c17d3129e8ec659dc5e02602e
murtaza1112 <54723469+murtaza1112@users.noreply.github.com>
2019-12-24 22:14:35 +0530

    Changes initialization file location

--- app/controllers/mypolls_controller.rb
+++ app/controllers/mypolls_controller.rb
@@ -1,6 +1,7 @@
 class MypollsController < ApplicationController
   before_action :set_mypoll, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:show,:index,:destroy]
+  @pcount=0
   # GET /mypolls
   # GET /mypolls.json
   def index
@@ -33,13 +34,15 @@ class MypollsController < ApplicationController
   # POST /mypolls
   # POST /mypolls.json
   def create
+    
     @mypoll = Mypoll.new(mypoll_params)
     @mypoll.user = current_user
+     @sur = current_user
+     @sur.count+=1
      @category=Category.find(@mypoll.category_id)
         @category.count+=1
-
         respond_to do |format|
-      if @mypoll.save and @category.save
+      if @mypoll.save and @category.save and @sur.save
        
         
         format.html { redirect_to @mypoll, notice: 'Mypoll was successfully created.' }
@@ -68,9 +71,15 @@ class MypollsController < ApplicationController
   # DELETE /mypolls/1
   # DELETE /mypolls/1.json
   def destroy
-    @mypoll.destroy
+    @category=Category.find(@mypoll.category_id)
+        if @category.count>0
+        @category.count-=1
+        @category.save
+        end
+        @mypoll.destroy
+    
     respond_to do |format|
-      format.html { redirect_to mypolls_url, notice: 'Mypoll was successfully destroyed.' }
+      format.html { redirect_to mypolls_url}
       format.json { head :no_content }
     end
   end
